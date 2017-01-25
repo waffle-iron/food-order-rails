ef2c13f5e92ab3addbcf2f3496f6b441a168ea43
aviisekh <aviisekh@gmail.com>
2017-01-25 16:31:00 +0545

    added configuration for simplecov in rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,25 @@ require 'rspec/rails'
 require 'shoulda/matchers'
 require 'capybara/rspec'
 require 'factory_girl_rails'
+
+require 'simplecov'
+require 'codeclimate-test-reporter'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov::Formatter::HTMLFormatter,
+CodeClimate::TestReporter::Formatter
+]
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start do
+  add_filter '/vendor/'
+  add_filter '/config/'
+  add_filter '/spec/'
+  add_filter '/db/'
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
